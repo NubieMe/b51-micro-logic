9017b5a5879a5eae90ad601a8425845626733a14
unknown <fahmi.mellow@gmail.com>
2024-01-17 11:53:12 +0700

    Refactor Salary

--- src/pages/Salary.jsx
+++ src/pages/Salary.jsx
@@ -8,46 +8,34 @@ const Salary = () => {
     const [salary, setSalary] = useState("")
     const [incentive, setIncentive] = useState("")
     const [tax, setTax] = useState("")
+    const [gross, setGross] = useState("")
+    const [net, setNet] =useState("")
 
     let Rupiah = new Intl.NumberFormat("id-ID", {
         style: "currency",
         currency: "IDR"
     })
 
-    const gross = () => {
-        const sum = parseInt(salary) + parseInt(incentive)
-        return Rupiah.format(sum)
-    }
-
-    const net = () => {
-        const tx = (parseInt(salary) + parseInt(incentive)) / 100 * parseInt(tax)
-        const sum = parseInt(salary) + parseInt(incentive) - tx
-        return Rupiah.format(sum)
-    }
-
+    
     function handle() {
-        const sal = document.getElementById("salary").value
-        const inc = document.getElementById("incentive").value
-        const tx = document.getElementById("tax").value
+        if(!salary | Math.sign(salary) === -1) return alert("Please input salary correctly!")
         
-        if (!inc | Math.sign(inc) === -1) return alert("Please input incentive correctly!")
+        if (!incentive | Math.sign(incentive) === -1) return alert("Please input incentive correctly!")
         
-        if(!tx | Math.sign(tx) === -1 | tx === 100) return alert("Please input tax correctly!")
+        if(!tax | Math.sign(tax) === -1 | tax === 100) return alert("Please input tax correctly!")
         
-        if(!sal | Math.sign(tx) === -1) return alert("Please input salary correctly!")
-        
-        setIncentive(inc)
-        setTax(tx)
+        const tx = (parseInt(salary) + parseInt(incentive)) / 100 * parseInt(tax)
+
+        setGross(parseInt(salary) + parseInt(incentive))
+        setNet(parseInt(salary) + parseInt(incentive) - tx)
     }
     
     function reset() {
-        document.getElementById("salary").value = ""
-        document.getElementById("incentive").value = ""
-        document.getElementById("tax").value = ""
-        
         setSalary("")
         setIncentive("")
         setTax("")
+        setGross("")
+        setNet("")
     }
 
     return (
@@ -58,19 +46,28 @@ const Salary = () => {
             <div style={subCon_styles} className="d-flex flex-wrap py-5 px-4 mt-5 justify-content-evenly gap-5 rounded">
                 <div className="d-flex flex-column bg-transparent text-light">
                     <label className="form-label bg-transparent">Salary</label>
-                    <input className="form-control mb-3" id="salary" type="number" onChange={e => setSalary(e.target.value)}/>
+                    <input className="form-control mb-3" id="salary" type="number" value={salary}
+                    onChange={e => setSalary(e.target.value)}/>
                     <label className="form-label bg-transparent">Incentives</label>
-                    <input className="form-control mb-3" id="incentive" type="number"/>
+                    <input className="form-control mb-3" id="incentive" type="number" value={incentive}
+                    onChange={e => setIncentive(e.target.value)}/>
                     <label className="form-label bg-transparent">Tax</label>
-                    <input className="form-control mb-3" id="tax" type="number" placeholder="in percent"/>
+                    <input className="form-control mb-3" id="tax" type="number" value={tax}
+                    onChange={e => setTax(e.target.value)} placeholder="in percent"/>
                     <button className="btn btn-primary" onClick={() => handle()}>Count</button>
                 </div>
                 <div className="d-flex flex-column justify-content-start bg-transparent text-light">
                     <h3 className="bg-transparent mb-4">Result</h3>
-                    <p id="gross" className="bg-transparent">Gross Salary: {incentive === "" ? "Rp. 0,00" : gross()}</p>
-                    <p id="main" className="bg-transparent">Main Salary: {salary === "" ? "Rp. 0,00" : Rupiah.format(salary)}</p>
-                    <p id="net" className="bg-transparent">Net Salary: {tax === "" ? "Rp. 0,00" : net()}</p>
-                    {tax === "" ? null : (
+                    <p id="gross" className="bg-transparent">
+                        Gross Salary: {gross === "" ? "Rp. 0,00" : gross}
+                    </p>
+                    <p id="main" className="bg-transparent">
+                        Main Salary: {salary === "" ? "Rp. 0,00" : Rupiah.format(salary)}
+                    </p>
+                    <p id="net" className="bg-transparent">
+                        Net Salary: {net === "" ? "Rp. 0,00" : net}
+                    </p>
+                    {net === "" ? null : (
                         <button className="btn btn-danger mt-4" onClick={() => reset()}>Reset</button>
                     )}
                 </div>
